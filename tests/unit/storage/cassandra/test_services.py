4bac59ccfba32a5cef2e058ca0ea3fbb875590f3
amitgandhinz <amit@gandhi.co.nz>
2014-07-08 18:04:23 -0400

    added basic unit test for get

--- tests/unit/storage/cassandra/test_services.py
+++ tests/unit/storage/cassandra/test_services.py
@@ -1,7 +1,7 @@
-from cdn.storage.cassandra.services import ServicesController
-from mock import patch, Mock
 import cassandra
-
+import json
+from cdn.storage.cassandra.services import ServicesController
+from mock import patch
 from unittest import TestCase
 
 
@@ -13,9 +13,25 @@ class CassandraStorageTests(TestCase):
         project_id = 123456
         service_name = 'mock_service'
 
+        cassandra_response = []
+        cassandra_response.append(project_id)
+        cassandra_response.append(service_name)
+
+        domains = ["{domain: 'www.mocksite.com'}"]
+        origins = ["{origin: 'www.mocksite.com'}"]
+        caching_rules = ["{name: 'www.mocksite.com'}"]
+        restrictions = ["{rules: 'www.mocksite.com'}"]
+
+        cassandra_response.append(domains)
+        cassandra_response.append(origins)
+        cassandra_response.append(caching_rules)
+        cassandra_response.append(restrictions)
+
+        # mock the response from cassandra
+        mock_execute.execute.return_value = json.dumps([cassandra_response])
+
         sc = ServicesController(None)
-        sc.get(project_id, service_name)
+        actual_response = json.loads(sc.get(project_id, service_name))
 
-        mock_execute.execute.assert_called_with(args={project_id})
-
-        pass
+        self.assertEqual(actual_response[0][0], project_id)
+        self.assertEqual(actual_response[0][1], service_name)
