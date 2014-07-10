ba20d9fb276832bea63dfd4f7cab20c656545611
amitgandhinz <amit@gandhi.co.nz>
2014-07-10 17:17:14 -0400

    cassandra unit tests

--- tests/unit/storage/cassandra/test_driver.py
+++ tests/unit/storage/cassandra/test_driver.py
@@ -15,46 +15,36 @@ CASSANDRA_OPTIONS = [
 
 
 class CassandraStorageServiceTests(TestCase):
-
     @patch.object(driver, 'CASSANDRA_OPTIONS', new=CASSANDRA_OPTIONS)
-    def test_storage_driver(self):
+    def setUp(self):
         conf = cfg.ConfigOpts()
-        cassandra_driver = driver.StorageDriver(conf, None)
+        self.cassandra_driver = driver.StorageDriver(conf, None)
 
+    def test_storage_driver(self):
         # assert that the configs are set up based on what was passed in
-        self.assertEquals(cassandra_driver.cassandra_conf['cluster'],
+        self.assertEquals(self.cassandra_driver.cassandra_conf['cluster'],
                           ['mock_ip'])
-        self.assertEquals(cassandra_driver.cassandra_conf.keyspace,
+        self.assertEquals(self.cassandra_driver.cassandra_conf.keyspace,
                           'mock_cdn')
 
     def test_is_alive(self):
-        conf = cfg.ConfigOpts()
-        cassandra_driver = driver.StorageDriver(conf, None)
-
-        self.assertEquals(cassandra_driver.is_alive(), True)
+        self.assertEquals(self.cassandra_driver.is_alive(), True)
 
     @patch.object(cassandra.cluster.Cluster, 'connect')
-    @patch.object(driver, 'CASSANDRA_OPTIONS', new=CASSANDRA_OPTIONS)
     def test_connection(self, mock_cluster):
-        conf = cfg.ConfigOpts()
-        cassandra_driver = driver.StorageDriver(conf, None)
-
-        cassandra_driver.connection()
+        self.cassandra_driver.connection()
         mock_cluster.assert_called_with('mock_cdn')
 
     def test_service_controller(self):
-        conf = cfg.ConfigOpts()
-        cassandra_driver = driver.StorageDriver(conf, None)
-
-        sc = cassandra_driver.service_controller
+        sc = self.cassandra_driver.service_controller
 
         self.assertIsInstance(sc, services.ServicesController)
 
     @patch.object(cassandra.cluster.Cluster, 'connect')
-    @patch.object(driver, 'CASSANDRA_OPTIONS', new=CASSANDRA_OPTIONS)
     def test_service_database(self, mock_cluster):
-        conf = cfg.ConfigOpts()
-        cassandra_driver = driver.StorageDriver(conf, None)
-
-        cassandra_driver.service_database
+        self.cassandra_driver.service_database
         mock_cluster.assert_called_with('mock_cdn')
+
+    def test_providers(self):
+        providers = self.cassandra_driver.providers
+        self.assertIsNone(providers)
