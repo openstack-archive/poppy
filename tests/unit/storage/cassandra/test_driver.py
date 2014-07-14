8aacb7edd21c1b9be6fb179e0df05c69425b41c0
amitgandhinz <amit@gandhi.co.nz>
2014-07-14 16:19:23 -0400

    py26 compatibility

--- tests/unit/storage/cassandra/test_driver.py
+++ tests/unit/storage/cassandra/test_driver.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import cassandra
 
 from cdn.storage.cassandra import driver
@@ -38,7 +53,9 @@ class CassandraStorageServiceTests(TestCase):
     def test_service_controller(self):
         sc = self.cassandra_driver.service_controller
 
-        self.assertIsInstance(sc, services.ServicesController)
+        self.assertEquals(
+            isinstance(sc, services.ServicesController),
+            True)
 
     @patch.object(cassandra.cluster.Cluster, 'connect')
     def test_service_database(self, mock_cluster):
@@ -47,4 +64,4 @@ class CassandraStorageServiceTests(TestCase):
 
     def test_providers(self):
         providers = self.cassandra_driver.providers
-        self.assertIsNone(providers)
+        self.assertEquals(providers, None)
--- tests/unit/storage/cassandra/test_services.py
+++ tests/unit/storage/cassandra/test_services.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import cassandra
 
 from cdn.storage.cassandra.driver import StorageDriver
@@ -91,4 +106,4 @@ class CassandraStorageServiceTests(TestCase):
     @patch.object(cassandra.cluster.Cluster, 'connect')
     def test_session(self, mock_service_database):
         session = self.sc.session
-        self.assertIsNotNone(session)
+        self.assertNotEquals(session, None)
