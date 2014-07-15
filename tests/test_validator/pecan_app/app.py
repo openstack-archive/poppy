ab368fc7c56af5cf2067ab216b0e5d8e23035f20
tonytan4ever <tonytan198211@gmail.com>
2014-07-15 14:13:02 -0400

    Missing an app file.

---
+++ tests/test_validator/pecan_app/app.py
@@ -0,0 +1,14 @@
+from pecan import make_app
+from pecan_test import model
+
+
+def setup_app(config):
+
+    model.init_model()
+    app_conf = dict(config.app)
+
+    return make_app(
+        app_conf.pop('root'),
+        logging=getattr(config, 'logging', {}),
+        **app_conf
+    )
