7ced7423a2abae30fa43b76bf1d293ccd2907d57
tonytan4ever <tonytan198211@gmail.com>
2014-07-23 11:17:01 -0400

    new style transport

--- cdn/transport/base.py
+++ cdn/transport/base.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Rackspace, Inc.
+# Copyright (c) 2014 Rackspace, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import abc
+
 import six
 
 
