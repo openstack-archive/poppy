1d2ed23c57bb5100c7a9e1c858cef0ee2259fc64
amitgandhinz <amit@gandhi.co.nz>
2014-07-25 16:16:31 -0400

    changed import order

--- tests/base.py
+++ tests/base.py
@@ -13,12 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 import fixtures
-from oslo.config import cfg
+import os
 import six
 import testtools
+
+from oslo.config import cfg
 
 
 class TestCase(testtools.TestCase):
