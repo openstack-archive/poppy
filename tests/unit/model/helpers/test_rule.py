6c4bfa085724daa210d9d969798198ce28567eff
Obulapathi N Challa <obulpathi@gmail.com>
2014-08-05 10:08:07 -0400

    Models and unittests for models

---
+++ tests/unit/model/helpers/test_rule.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import ddt
+
+from cdn.model.helpers import rule
+from tests.unit import base
+
+
+@ddt.ddt
+class TestRule(base.TestCase):
+
+    def test_rule(self):
+
+        name = 'name'
+        http_host = 'www.mywebsite.com'
+        client_ip = '192.168.1.1'
+        http_method = 'POST'
+        request_url = '/index.html'
+
+        myrule = rule.Rule(name)
+
+        # test all properties
+        # name
+        self.assertEqual(myrule.name, name)
+        self.assertRaises(AttributeError, setattr, myrule, 'name', name)
+
+        # http_host
+        self.assertEqual(myrule.http_host, None)
+        myrule.http_host = http_host
+        self.assertEqual(myrule.http_host, http_host)
+
+        # client_ip
+        self.assertEqual(myrule.client_ip, None)
+        myrule.client_ip = client_ip
+        self.assertEqual(myrule.client_ip, client_ip)
+
+        # http_method
+        self.assertEqual(myrule.http_method, None)
+        myrule.http_method = http_method
+        self.assertEqual(myrule.http_method, http_method)
+
+        # request_url
+        self.assertEqual(myrule.request_url, None)
+        myrule.request_url = request_url
+        self.assertEqual(myrule.request_url, request_url)
---
+++ tests/unit/model/test_service.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import ddt
+
+from cdn.model.helpers import domain
+from cdn.model.helpers import origin
+from cdn.model import service
+
+from tests.unit import base
+
+
+@ddt.ddt
+class TestService(base.TestCase):
+
+    def test_create_service(self):
+        service_name = 'NewService'
+        myorigins = []
+        mydomains = []
+
+        myorigins.append(origin.Origin('mysite.com'))
+        myorigins.append(origin.Origin('yoursite.io', port=80, ssl=True))
+
+        mydomains.append(domain.Domain('oursite.org'))
+        mydomains.append(domain.Domain('wiki.cc'))
+
+        myservice = service.Service(service_name, mydomains, myorigins)
+
+        # test all properties
+        # name
+        self.assertEqual(myservice.name, service_name)
+        self.assertRaises(
+            AttributeError, setattr, myservice, 'name', service_name)
+
+        # domains
+        self.assertEqual(myservice.domains, mydomains)
+        self.assertRaises(AttributeError, setattr, myservice, 'domains', [])
+
+        # origins
+        self.assertEqual(myservice.origins, myorigins)
+        self.assertRaises(AttributeError, setattr, myservice, 'origins', [])
