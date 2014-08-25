c466b6ed23f462eb9cfe15180fd2c1309ceb08a5
Malini Kamalambal <malini.kamalambal@rackspace.com>
2014-08-25 11:20:43 -0400

    Add API Tests for flavors
    This patch add the basic API tests for create, get and delete
    flavors.
    
    Implements: blueprint flavors
    Change-Id: I7376ae1a966656ee6e01c8e0f782d563a11837b4

--- tests/api/utils/client.py
+++ tests/api/utils/client.py
@@ -109,3 +109,48 @@ class PoppyClient(client.AutoMarshallingHTTPClient):
 
         url = '{0}/services/{1}'.format(self.url, service_name)
         return self.request('DELETE', url)
+
+    def create_flavor(self, flavor_id=None, provider_list=None,
+                      requestslib_kwargs=None):
+        """Create flavor
+
+        :return: Response Object containing response code 204 and header with
+                 Location
+        PUT
+        flavors/{flavor_id}
+        """
+        url = '{0}/flavors/{1}'.format(self.url, flavor_id)
+        request_object = requests.CreateFlavor(flavor_id=flavor_id,
+                                               provider_list=provider_list)
+        return self.request('PUT', url,
+                            request_entity=request_object,
+                            requestslib_kwargs=requestslib_kwargs)
+
+    def get_flavor(self, flavor_location=None, flavor_id=None):
+        """Get Flavor
+
+        :return: Response Object containing response code 200 and body with
+        details of flavor
+        GET
+        flavors/{flavor_id}
+        """
+        if flavor_location:
+            url = '{0}{1}'.format(self.url, flavor_location)
+        else:
+            url = '{0}/flavors/{1}'.format(self.url, flavor_id)
+
+        return self.request('GET', url)
+
+    def delete_flavor(self, flavor_location=None, flavor_id=None):
+        """Delete Flavor
+
+        :return: Response Object containing response code 204
+        DELETE
+        flavors/{flavor_id}
+        """
+        if flavor_location:
+            url = '{0}{1}'.format(self.url, flavor_location)
+        else:
+            url = '{0}/flavors/{1}'.format(self.url, flavor_id)
+
+        return self.request('DELETE', url)
