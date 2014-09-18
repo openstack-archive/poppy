ab4763f3acda20f344514fbc9a20c90bbdc50ad6
tonytan4ever <tonytan198211@gmail.com>
2014-09-18 16:20:04 -0400

    Get Service (get one endpoint)
    Change-Id: Ibd900a171239747169c99526c411f8a9ce7a20e7

--- poppy/storage/mockdb/services.py
+++ poppy/storage/mockdb/services.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+
 from poppy.model.helpers import domain
 from poppy.model.helpers import origin
 from poppy.model.helpers import provider_details
@@ -27,117 +29,131 @@ class ServicesController(base.ServicesController):
         return self._driver.database
 
     def list(self, project_id, marker=None, limit=None):
-        services = [
-            {
-                "name": "mockdb1_service_name",
-                "domains": [
-                        {
-                            "domain": "www.mywebsite.com"
-                        }
-                ],
-                "origins": [
-                    {
-                        "origin": "mywebsite.com",
-                        "port": 80,
-                        "ssl": False
-                    }
-                ],
-                "caching": [
-                    {"name": "default", "ttl": 3600},
-                    {
-                        "name": "home",
-                        "ttl": 17200,
-                        "rules": [
-                                {"name": "index",
-                                 "request_url": "/index.htm"}
-                        ]
-                    },
-                    {
-                        "name": "images",
-                        "ttl": 12800,
-                        "rules": [
-                                {"name": "images", "request_url": "*.png"}
-                        ]
-                    }
-                ],
-                "restrictions": [
-                    {
-                        "name": "website only",
-                        "rules": [
-                                {
-                                    "name": "mywebsite.com",
-                                    "http_host": "www.mywebsite.com"
-                                }
-                        ]
-                    }
-                ],
-            }
-        ]
+        services = [{"name": "mockdb1_service_name",
+                     "domains": [{"domain": "www.mywebsite.com"}],
+                     "origins": [{"origin": "mywebsite.com",
+                                  "port": 80,
+                                  "ssl": False}],
+                     "caching": [{"name": "default",
+                                  "ttl": 3600},
+                                 {"name": "home",
+                                  "ttl": 17200,
+                                  "rules": [{"name": "index",
+                                             "request_url": "/index.htm"}]},
+                                 {"name": "images",
+                                  "ttl": 12800,
+                                  "rules": [{"name": "images",
+                                             "request_url": "*.png"}]}],
+                     "restrictions": [{"name": "website only",
+                                       "rules": [{"name": "mywebsite.com",
+                                                  "http_host":
+                                                  "www.mywebsite.com"}]}],
+                     "provider_details":
+                     {
+                         "MaxCDN":
+                         "{\"id\": 11942,"
+                         " \"access_urls\": "
+                         "[\"mypullzone.netdata.com\"]}",
+                         "Mock": "{\"id\": 73242,"
+                         " \"access_urls\": [\"mycdn.mock.com\"]}",
+                         "CloudFront":
+                         "{\"id\": \"5ABC892\","
+                         " \"access_urls\": "
+                         "[\"cf123.cloudcf.com\"]}",
+                         "Fastly":
+                         "{\"id\": 3488,"
+                         " \"access_urls\": "
+                         "[\"mockcf123.fastly.prod.com\"]}"
+                     }}
+                    ]
 
         services_result = []
         for r in services:
             name = r.get("name", "unnamed")
             origins = r.get("origins", [])
             domains = r.get("domains", [])
+            provider_detail_dicts = r.get("provider_details")
             origins = [origin.Origin(d) for d in origins]
             domains = [domain.Domain(d) for d in domains]
-            services_result.append(service.Service(name, domains, origins))
+            provider_details_dict = {}
+            for provider_name in provider_detail_dicts:
+                provider_detail_dict = json.loads(
+                    provider_detail_dicts[provider_name])
+                provider_service_id = provider_detail_dict.get("id", None)
+                access_urls = provider_detail_dict.get("access_urls", [])
+                status = provider_detail_dict.get("status", u'unknown')
+                provider_detail_obj = provider_details.ProviderDetail(
+                    provider_service_id=provider_service_id,
+                    access_urls=access_urls,
+                    status=status)
+                provider_details_dict[provider_name] = provider_detail_obj
+            service_result = service.Service(name, domains, origins)
+            service_result.provider_details = provider_details_dict
+            services_result.append(service_result)
 
         return services_result
 
     def get(self, project_id, service_name):
         # get the requested service from storage
-        service_dict = {
-            "name": service_name,
-            "domains": [
-                {
-                    "domain": "www.mywebsite.com"
-                }
-            ],
-            "origins": [
-                {
-                    "origin": "mywebsite.com",
-                    "port": 80,
-                    "ssl": False
-                }
-            ],
-            "caching": [
-                {"name": "default", "ttl": 3600},
-                {
-                    "name": "home",
-                            "ttl": 17200,
-                            "rules": [
-                                {"name": "index",
-                                 "request_url": "/index.htm"}
-                            ]
-                },
-                {
-                    "name": "images",
-                            "ttl": 12800,
-                            "rules": [
-                                {"name": "images", "request_url": "*.png"}
-                            ]
-                }
-            ],
-            "restrictions": [
-                {
-                    "name": "website only",
-                            "rules": [
-                                {
-                                    "name": "mywebsite.com",
-                                    "http_host": "www.mywebsite.com"
-                                }
-                            ]
-                }
-            ],
-        }
+        service_dict = {"name": service_name,
+                        "domains": [{"domain": "www.mywebsite.com"}],
+                        "origins": [{"origin": "mywebsite.com",
+                                     "port": 80,
+                                     "ssl": False}],
+                        "caching": [{"name": "default",
+                                     "ttl": 3600},
+                                    {"name": "home",
+                                     "ttl": 17200,
+                                     "rules": [{"name": "index",
+                                                "request_url": "/index.htm"}]},
+                                    {"name": "images",
+                                     "ttl": 12800,
+                                     "rules": [{"name": "images",
+                                                "request_url": "*.png"}]}],
+                        "restrictions": [{"name": "website only",
+                                          "rules": [{"name": "mywebsite.com",
+                                                     "http_host":
+                                                     "www.mywebsite.com"}]}],
+                        "provider_details":
+                        {
+                            "MaxCDN":
+                            "{\"id\": 11942,"
+                            " \"access_urls\": "
+                            "[\"mypullzone.netdata.com\"]}",
+                            "Mock":
+                            "{\"id\": 73242,"
+                            " \"access_urls\": "
+                            "[\"mycdn.mock.com\"]}",
+                            "CloudFront":
+                            "{\"id\": \"5ABC892\","
+                            " \"access_urls\": "
+                            "[\"cf123.cloudcf.com\"]}",
+                            "Fastly":
+                            "{\"id\": 3488,"
+                            " \"access_urls\": "
+                            "[\"mockcf123.fastly.prod.com\""
+                            "]}"}}
 
         name = service_dict.get("name", "unnamed")
         origins = service_dict.get("origins", [])
         domains = service_dict.get("domains", [])
         origins = [origin.Origin(d) for d in origins]
         domains = [domain.Domain(d) for d in domains]
+        provider_detail_dicts = service_dict.get("provider_details")
         services_result = service.Service(name, domains, origins)
+        provider_details_dict = {}
+        for provider_name in provider_detail_dicts:
+            provider_detail_dict = json.loads(
+                provider_detail_dicts[provider_name])
+            provider_service_id = provider_detail_dict.get("id", None)
+            access_urls = provider_detail_dict.get("access_urls", [])
+            status = provider_detail_dict.get("status", u'unknown')
+            provider_detail_obj = provider_details.ProviderDetail(
+                provider_service_id=provider_service_id,
+                access_urls=access_urls,
+                status=status)
+            provider_details_dict[provider_name] = provider_detail_obj
+        services_result.provider_details = provider_details_dict
         return services_result
 
     def create(self, project_id, service_name, service_json):
@@ -156,19 +172,19 @@ class ServicesController(base.ServicesController):
     def get_provider_details(self, project_id, service_name):
         return {
             "MaxCDN": provider_details.ProviderDetail(
-                id=11942,
+                provider_service_id=11942,
                 name='my_service_name',
-                access_url='my_service_name'
-                '.mycompanyalias.netdna-cdn.com'),
+                access_urls=['my_service_name'
+                             '.mycompanyalias.netdna-cdn.com']),
             "Fastly": provider_details.ProviderDetail(
-                id=3488,
+                provider_service_id=3488,
                 name="my_service_name",
-                access_url='my_service_name'
-                '.global.prod.fastly.net'),
+                access_urls=['my_service_name'
+                             '.global.prod.fastly.net']),
             "CloudFront": provider_details.ProviderDetail(
-                id=5892,
-                access_url='my_service_name'
-                '.gibberish.amzcf.com'),
+                provider_service_id=5892,
+                access_urls=['my_service_name'
+                             '.gibberish.amzcf.com']),
             "Mock": provider_details.ProviderDetail(
-                id="73242",
-                access_url='my_service_name.mock.com')}
+                provider_service_id="73242",
+                access_urls=['my_service_name.mock.com'])}
