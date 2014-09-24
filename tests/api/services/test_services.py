5d8d117e8bb90571dbf023e6c945b498f09d2d11
Malini Kamalambal <malini.kamalambal@rackspace.com>
2014-09-24 07:38:08 -0400

    API Tests for Create Service endpoint
    This patch updates API Tests for Create Service enpoint, to include
    Fastly service validation.
    
    Change-Id: I6c3370b5dd8937f4349e10ca3e81ae3c2f5eada3

--- tests/api/services/test_services.py
+++ tests/api/services/test_services.py
@@ -17,45 +17,71 @@ import uuid
 
 import ddt
 
-from tests.api import base
-from tests.api.utils.schema import response
+from tests.api import providers
+# from tests.api.utils.schema import response - Uncomment after get_service API
 
 
 @ddt.ddt
-class TestServices(base.TestBase):
+class TestServices(providers.TestProviderBase):
 
     """Tests for Services."""
 
     def setUp(self):
         super(TestServices, self).setUp()
-        self.service_name = uuid.uuid1()
+        self.service_name = str(uuid.uuid1())
 
     @ddt.file_data('data_create_service.json')
     def test_create_service(self, test_data):
 
-        self.skipTest('Endpoint Not Implemented')
-
         domain_list = test_data['domain_list']
         origin_list = test_data['origin_list']
         caching_list = test_data['caching_list']
+        flavor = test_data['flavorRef']
 
         resp = self.client.create_service(service_name=self.service_name,
                                           domain_list=domain_list,
                                           origin_list=origin_list,
-                                          caching_list=caching_list)
-        self.assertEqual(resp.status_code, 201)
+                                          caching_list=caching_list,
+                                          flavorRef=flavor)
+        self.assertEqual(resp.status_code, 202)
 
-        response_body = resp.json()
-        self.assertSchema(response_body, response.create_service)
-
+        # TODO(malini): uncomment after get_service endpoint is complete.
+        '''
         # Get on Created Service
         resp = self.client.get_service(service_name=self.service_name)
         self.assertEqual(resp.status_code, 200)
 
         body = resp.json()
+        self.assertSchema(body, response.create_service)
+
         self.assertEqual(body['domains'], domain_list)
         self.assertEqual(body['origins'], origin_list)
         self.assertEqual(body['caching_list'], caching_list)
+        '''
+
+        # Verify the service is updated at all Providers for the flavor
+        if self.test_config.provider_validation:
+            service_details = (
+                self.getServiceFromFlavor(flavor, self.service_name))
+            provider_list = self.config.flavor[flavor]
+            # Verify that the service stored in each provider (that is part of
+            # the flavor) is what Poppy sent them.
+            for provider in provider_list:
+                self.assertEqual(
+                    sorted(service_details[provider]['domain_list']),
+                    sorted(domain_list),
+                    msg='Domain Lists Not Correct for {0} service name {1}'.
+                        format(provider, self.service_name))
+                self.assertEqual(
+                    sorted(service_details[provider]['origin_list']),
+                    sorted(origin_list),
+                    msg='Origin List Not Correct for {0} service name {1}'.
+                        format(provider, self.service_name))
+                self.assertEqual(
+                    sorted(service_details[provider]['caching_list']),
+                    sorted(caching_list),
+                    msg='Caching List Not Correct for {0} service name {1}'.
+                        format(provider, self.service_name))
 
     def tearDown(self):
         self.client.delete_service(service_name=self.service_name)
