e2460bb3ec6c85f96dabb998d6528237ae4a176b
Malini Kamalambal <malini.kamalambal@rackspace.com>
2014-09-24 07:38:08 -0400

    API Tests for Create Service endpoint
    This patch adds API Tests for Create Service enpoint, including
    provider side validation.
    
    Change-Id: I6c3370b5dd8937f4349e10ca3e81ae3c2f5eada3

--- tests/api/services/test_services.py
+++ tests/api/services/test_services.py
@@ -28,34 +28,40 @@ class TestServices(base.TestBase):
 
     def setUp(self):
         super(TestServices, self).setUp()
-        self.service_name = uuid.uuid1()
+        self.service_name = str(uuid.uuid1())
 
     @ddt.file_data('data_create_service.json')
     def test_create_service(self, test_data):
 
-        self.skipTest('Endpoint Not Implemented')
-
         domain_list = test_data['domain_list']
         origin_list = test_data['origin_list']
         caching_list = test_data['caching_list']
+        flavor = test_data['flavorRef']
 
         resp = self.client.create_service(service_name=self.service_name,
                                           domain_list=domain_list,
                                           origin_list=origin_list,
-                                          caching_list=caching_list)
-        self.assertEqual(resp.status_code, 201)
+                                          caching_list=caching_list,
+                                          flavorRef=flavor)
+        self.assertEqual(resp.status_code, 202)
 
-        response_body = resp.json()
-        self.assertSchema(response_body, response.create_service)
-
+        '''
         # Get on Created Service
         resp = self.client.get_service(service_name=self.service_name)
         self.assertEqual(resp.status_code, 200)
 
         body = resp.json()
+        self.assertSchema(body, response.create_service)
+
         self.assertEqual(body['domains'], domain_list)
         self.assertEqual(body['origins'], origin_list)
         self.assertEqual(body['caching_list'], caching_list)
+        '''
+
+        # Verify the service is updated at all Providers for the flavor
+        if self.test_config.provider_validation:
+            import time; time.sleep(2)
+            self.assertFlavorUpdated(flavor, self.service_name)
 
     def tearDown(self):
         self.client.delete_service(service_name=self.service_name)
---
+++ tests/api/utils/fastlyclient.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cafe.engine.http import client
+import fastly
+
+
+class FastlyClient(client.AutoMarshallingHTTPClient):
+
+    """Client objects for Fastly api calls."""
+
+    def __init__(self, api_key, email, password, serialize_format="json",
+                 deserialize_format="json"):
+        super(FastlyClient, self).__init__(serialize_format,
+                                           deserialize_format)
+        self.client = fastly.connect(api_key)
+        self.client.login(email, password)
+
+        self.serialize = serialize_format
+        self.deserialize_format = deserialize_format
+
+    def get_service(self, service_name):
+        # Get the service
+        service = self.client.get_service_by_name(service_name)
+        service_version = self.client.list_versions(service.id)
+
+        # The create service api_call updates the domain, origin & cache
+        # settings in second version of the service. The version # will be the
+        # value of 'number' in the second element of the list returned by
+        # self.client.list_versions(service.id) call above
+        version = service_version[1].number
+
+        # Get the Domain List
+        domains = self.client.list_domains(service.id, version)
+        domain_list = [domain.name for domain in domains]
+
+        # Get the Cache List
+        cache_setting_list = self.client.list_cache_settings(
+            service.id, version)
+
+        cache_list = [{'name': item['name'], 'ttl': int(item['ttl']),
+                       'rules': item['cache_condition']}
+                      for item in cache_setting_list]
+
+        # Get the Origin List
+        backends = self.client.list_backends(service.id, version)
+        origin = backends[0].address
+        port = backends[0].port
+        ssl = backends[0].use_ssl
+
+        origin_list = [{'origin': origin, 'port': port, 'ssl': ssl}]
+
+        return (domain_list, origin_list, cache_list)
+
+    '''except fastly.FastlyError:
+        print('1', fastly.FastlyError)
+        return ("failed to GET service")
+    except Exception:
+        print('2', Exception)
+        return ("failed to GET service")
+    '''
