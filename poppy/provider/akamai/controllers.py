ccea5863ab3b6a048b4223f579e5ec69a92fab11
tonytan4ever <tonytan198211@gmail.com>
2014-10-22 12:13:48 -0400

    Implementing Akamai Driver
    Change-Id: I822c0248995be0af3a880445b113225d26435698

--- poppy/provider/akamai/controllers.py
+++ poppy/provider/akamai/controllers.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Rackspace, Inc.
+# Copyright (c) 2013 Rackspace, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,15 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from poppy.model.helpers import rule
+"""Exports Akamai poppy controllers.
 
+Field Mappings:
+    In order to reduce the disk / memory space used,
+    fields name will be, most of the time, the first
+    letter of their long name. Fields mapping will be
+    updated and documented in each controller class.
+"""
 
-def load_from_json(json_data):
-    name = json_data.get('name')
-    res = rule.Rule(name)
-    res.referrer = json_data.get('referrer', None)
-    res.http_host = json_data.get('http_host', None)
-    res.http_method = json_data.get('http_method', None)
-    res.client_ip = json_data.get('client_ip', None)
-    res.request_url = json_data.get('request_url', None)
-    return res
+from poppy.provider.akamai import services
+
+ServiceController = services.ServiceController
