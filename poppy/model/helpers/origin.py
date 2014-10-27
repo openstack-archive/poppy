ce97a0bc14a5deeafeca53044ce56347169e2b0b
Miguel Quintero <migmaqer@gmail.com>
2014-10-27 02:43:21 -0400

    blueprint: add Docstrings to all things, part2
    Change-Id: I454b0bba3bf1f4cec17ab8e1e59f1c0c265cba24

--- poppy/model/helpers/origin.py
+++ poppy/model/helpers/origin.py
@@ -17,6 +17,7 @@ from poppy.model import common
 
 
 class Origin(common.DictSerializableModel):
+    """Origin."""
 
     def __init__(self, origin, port=80, ssl=False):
         self._origin = origin
@@ -26,34 +27,51 @@ class Origin(common.DictSerializableModel):
 
     @property
     def origin(self):
+        """origin."""
         return self._origin
 
     @origin.setter
     def origin(self, value):
+        """origin setter."""
         self._origin = value
 
     @property
     def port(self):
+        """port.
+
+        :returns port
+        """
         return self._port
 
     @port.setter
     def port(self, value):
+        """port setter."""
         self._port = value
 
     @property
     def ssl(self):
+        """self.
+
+        :returns ssl
+        """
         return self._ssl
 
     @ssl.setter
     def ssl(self, value):
+        """ssl setter."""
         self._ssl = value
 
     @property
     def rules(self):
+        """rules.
+
+        :returns rules
+        """
         return self._rules
 
     @rules.setter
     def rules(self, value):
+        """rules setter."""
         # TODO(tonytan4ever) this field should by typed too
         self._rules = value
 
@@ -62,7 +80,11 @@ class Origin(common.DictSerializableModel):
         """Construct a model instance from a dictionary.
 
         This serves as a 2nd constructor
+
+        :param dict_obj: dictionary object
+        :returns o
         """
+
         o = cls("unnamed")
         o.origin = dict_obj.get("origin", "unnamed")
         o.port = dict_obj.get("port", 80)
