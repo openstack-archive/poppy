6dd7f804e67cd881a057aaf191a79e75efc0c45c
amitgandhinz <amit.gandhi@rackspace.com>
2014-11-18 14:23:40 -0500

    Added ability to run tests against a fig docker setup
    Change-Id: I406a78168c2058b3ed5b7a35c55496ae20d957da
    
    Added tox environment for running api tests against docker containers
    
    Change-Id: I5c775fda34412cfb7b6704060f38fdfa4bea43a3

--- setup.py
+++ setup.py
@@ -16,9 +16,12 @@
 
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import os
+import sys
 
 import pip
 import setuptools
+from setuptools.command.test import test as TestCommand
+
 
 requirement_files = []
 # walk the requirements directory and gather requirement files
@@ -36,7 +39,29 @@ for requirement_file in requirement_files:
 # convert requirements in to list
 requirements = list(requirements)
 
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
+
 setuptools.setup(
     install_requires=requirements,
     setup_requires=['pbr'],
-    pbr=True)
+    pbr=True,
+    tests_require=['tox'],
+    cmdclass={'test': Tox})
