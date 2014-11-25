5e6d9d25bf9bbd1098d6ae63befe5c9ad1d0e9f4
Malini Kamalambal <malini.kamalambal@rackspace.com>
2014-11-25 16:27:07 -0500

    Adds End To End Tests
    This patchset adds the end to end tests for Poppy.
    The test steps include,
    1) Deploying a WP site on a cloud server
    2) Creating a Poppy Service for the WP site
    3) Reporting page load metrics using webpagtest
    
    Change-Id: I5f96cd21ac59806310f314e27da4f07642d3cfb0

---
+++ tests/endtoend/test_cdn_website.py
@@ -0,0 +1,72 @@
+# coding= utf-8
+
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+import string
+import uuid
+
+from tests.endtoend import base
+
+
+class TestWebsiteCDN(base.TestBase):
+
+    """Tests for CDN enabling a website."""
+
+    def setUp(self):
+        super(TestWebsiteCDN, self).setUp()
+
+        def _random_string(length=12):
+            return ''.join([random.choice(string.ascii_letters)
+                            for _ in range(length)])
+
+        self.stack_name = _random_string()
+
+        self.domain_name = 'TestCDN-' + _random_string() + '.org'
+
+        self.heat_client.create_stack(stack_name=self.stack_name,
+                                      domain_name=self.domain_name)
+        print(self.stack_name)
+        print(self.domain_name)
+
+        self.heat_client.wait_for_stack_status(stack_name=self.stack_name)
+        self.origin = self.heat_client.get_server_ip(
+            stack_name=self.stack_name)
+
+    def test_enable_cdn(self):
+
+        domain_list = [{"domain": self.domain_name}]
+        origin_list = [{"origin": self.origin,
+                        "port": 80,
+                        "ssl": False}]
+        caching_list = []
+        flavor = 'standard'
+        service_name = str(uuid.uuid1())
+
+        resp = self.poppy_client.create_service(
+            service_name=service_name,
+            domain_list=domain_list,
+            origin_list=origin_list,
+            caching_list=caching_list,
+            flavor_ref=flavor)
+
+        self.assertEqual(resp.status_code, 202)
+        self.poppy_client.wait_for_service_status(service_name=service_name,
+                                                  status='DEPLOYED')
+
+    def tearDown(self):
+        #self.heat_client.delete_stack(stack_name=self.stack_name)
+        super(TestWebsiteCDN, self).tearDown()
