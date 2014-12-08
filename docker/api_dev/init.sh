85d822bc7619517d3c6a1f5c271c05faf61593e7
amitgandhinz <amit.gandhi@rackspace.com>
2014-12-08 18:10:33 -0500

    Added init script to inject credentials for DNS and Providers
    Change-Id: Id9b8454ac65cd566fa16b9863005b254af5a2eeb

---
+++ docker/api_dev/init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Setup Configs
+echo "Setting Configs"
+CONFIG=/etc/poppy.conf
+
+sed -i -e "s/DNS_USERNAME/$DNS_USERNAME/"                   $CONFIG
+sed -i -e "s/DNS_APIKEY/$DNS_APIKEY/"                       $CONFIG
+sed -i -e "s/DNS_URL/$DNS_URL/"                             $CONFIG
+sed -i -e "s/DNS_EMAIL/$DNS_EMAIL/"                         $CONFIG
+
+sed -i -e "s/FASTLY_APIKEY/$FASTLY_APIKEY/"                 $CONFIG
+
+sed -i -e "s/AKAM_POLICY_API_CLIENT_TOKEN/$AKAM_POLICY_API_CLIENT_TOKEN/"   $CONFIG
+sed -i -e "s/AKAM_POLICY_API_CLIENT_SECRET/$AKAM_POLICY_API_CLIENT_SECRET/" $CONFIG
+sed -i -e "s/AKAM_POLICY_API_ACCESS_TOKEN/$AKAM_POLICY_API_ACCESS_TOKEN/"   $CONFIG
+sed -i -e "s/AKAM_POLICY_API_BASE_URL/$AKAM_POLICY_API_BASE_URL/"           $CONFIG
+sed -i -e "s/AKAM_CCU_API_CLIENT_TOKEN/$AKAM_CCU_API_CLIENT_TOKEN/"         $CONFIG
+sed -i -e "s/AKAM_CCU_API_CLIENT_SECRET/$AKAM_CCU_API_CLIENT_SECRET/"       $CONFIG
+sed -i -e "s/AKAM_CCU_API_ACCESS_TOKEN/$AKAM_CCU_API_ACCESS_TOKEN/"         $CONFIG
+sed -i -e "s/AKAM_CCU_API_BASE_URL/$AKAM_CCU_API_BASE_URL/"                 $CONFIG
+sed -i -e "s/AKAM_ACCESS_URL_LINK/$AKAM_ACCESS_URL_LINK/"                   $CONFIG
+
+
+/usr/local/bin/uwsgi --ini /root/uwsgi.ini
