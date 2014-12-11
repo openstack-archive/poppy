ef2ae0bb3064a14b550155cc7445ebc96b405f46
Obulpathi <obulapathi.challa@rackspace.com>
2014-12-11 13:57:11 -0500

    Fixes bug: Failed Provider Service causes DNS create failure
    Change-Id: I8f0d66b473d7232415dad046c1bd3d6356824cc6

--- poppy/dns/rackspace/services.py
+++ poppy/dns/rackspace/services.py
@@ -123,6 +123,9 @@ class ServicesController(base.ServicesBase):
                     if link['rel'] == 'access_url':
                         links[link['domain']] = link['href']
 
+        if not links:
+            return self.responder.created({})
+
         # create CNAME records
         try:
             dns_links = self._create_cname_records(links)
@@ -145,7 +148,7 @@ class ServicesController(base.ServicesBase):
                             'provider_url': link['href'],
                             'operator_url': dns_links[link['href']]}
                         access_urls.append(access_url)
-            dns_details[provider_name] = {'access_urls': access_urls}
+                dns_details[provider_name] = {'access_urls': access_urls}
         return self.responder.created(dns_details)
 
     def delete(self, provider_details):
@@ -341,6 +344,6 @@ class ServicesController(base.ServicesBase):
                             'provider_url': link['href'],
                             'operator_url': operator_url}
                         access_urls.append(access_url)
-            dns_details[provider_name] = {'access_urls': access_urls}
+                dns_details[provider_name] = {'access_urls': access_urls}
 
         return self.responder.updated(dns_details)
