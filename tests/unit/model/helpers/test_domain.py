a88a08f94cb46eece49fa32a602a9a03d5db9286
tonytan4ever <tonytan198211@gmail.com>
2015-01-06 12:47:58 -0500

    add shared_ssl property to domain model
    Change-Id: I6e77f60869a70fcd45f2021ba0cbd565d42a09d8

--- tests/unit/model/helpers/test_domain.py
+++ tests/unit/model/helpers/test_domain.py
@@ -36,8 +36,14 @@ class TestDomain(base.TestCase):
         # test all properties
         # domain
         self.assertEqual(mydomain.domain, domain_name)
+        self.assertEqual(mydomain.protocol, 'http')
+        self.assertEqual(mydomain.shared_ssl, False)
         mydomain.domain = changed_domain_name
         self.assertEqual(mydomain.domain, changed_domain_name)
 
         my_other_domain = domain.Domain.init_from_dict({"domain": domain_name})
         self.assertEqual(my_other_domain.domain, domain_name)
+
+        my_https_domain = domain.Domain(domain_name, 'https', True)
+        self.assertEqual(my_https_domain.protocol, 'https')
+        self.assertEqual(my_https_domain.shared_ssl, True)
