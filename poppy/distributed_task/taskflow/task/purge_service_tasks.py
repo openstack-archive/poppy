e65593b3eba3f7acfb46b5f896c8f3f4ff688197
wbrothers <william.brothers@rackspace.com>
2015-01-09 16:45:11 +0000

    feat: Break down async tasks with TaskFlow
    - Each of Create Service, Delete Service, Purge Service and
      Update service now consistute of smaller tasks, contained within
      a Graph Flow
    
    - The DNS tasks have been in-built retry logic within them, by
      exponentially backing off, in case of a failure.
    
    Change-Id: I61f8ad613b97087f5ff03c0fc2ac4994258b655f

---
+++ poppy/distributed_task/taskflow/task/purge_service_tasks.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2015 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+from oslo.config import cfg
+from taskflow import task
+
+from poppy import bootstrap
+from poppy.openstack.common import log
+from poppy.transport.pecan.models.request import (
+    provider_details as req_provider_details)
+
+LOG = log.getLogger(__name__)
+
+conf = cfg.CONF
+conf(project='poppy', prog='poppy', args=[])
+
+
+class PurgeProviderServicesTask(task.Task):
+    default_provides = "responders"
+
+    def execute(self, provider_details, purge_url):
+        bootstrap_obj = bootstrap.Bootstrap(conf)
+        service_controller = bootstrap_obj.manager.services_controller
+        provider_details = json.loads(provider_details)
+        purge_url = None if purge_url == 'None' else purge_url
+
+        responders = []
+        # try to purge all service from each provider presented
+        # in provider_details
+        for provider in provider_details:
+            # NOTE(tonytan4ever): if the purge_url is None, it means to purge
+            # all content, else only purge a specific purge url
+            provider_details[provider] = (
+                req_provider_details.load_from_json(
+                    provider_details[provider]))
+            LOG.info('Starting to purge service from %s, purge_url: %s' %
+                     (provider,
+                      'all' if purge_url is None else purge_url))
+            responder = service_controller.provider_wrapper.purge(
+                service_controller._driver.providers[provider.lower()],
+                provider_details,
+                purge_url)
+            responders.append(responder)
+            LOG.info('Purge service %s  on  %s complete...' %
+                     (provider,
+                      'all' if purge_url is None else purge_url))
+
+        return responders
