d4f60470862407ce28246f603830a5a5a766ed65
Obulpathi <obulapathi.challa@rackspace.com>
2015-01-15 11:13:47 -0500

    Adds tests for Rackspace DNS
    Change-Id: I9bb4f61600f0d5759477f0eaa0315ae39506e837

--- tests/unit/dns/rackspace/test_services.py
+++ tests/unit/dns/rackspace/test_services.py
@@ -18,7 +18,7 @@ import uuid
 import ddt
 import mock
 from oslo.config import cfg
-import six
+import pyrax.exceptions as exc
 
 from poppy.dns.rackspace import driver
 from poppy.model.helpers import domain
@@ -32,10 +32,10 @@ RACKSPACE_OPTIONS = [
                help='Keystone API Key'),
     cfg.BoolOpt('sharding_enabled', default=True,
                 help='Enable Sharding?'),
-    cfg.IntOpt('num_shards', default=10, help='Number of Shards to use'),
+    cfg.IntOpt('num_shards', default=500, help='Number of Shards to use'),
     cfg.StrOpt('shard_prefix', default='cdn',
                help='The shard prefix to use'),
-    cfg.StrOpt('url', default='',
+    cfg.StrOpt('url', default='rackcdn.com',
                help='The url for customers to CNAME to'),
     cfg.StrOpt('email', help='The email to be provided to Rackspace DNS for'
                'creating subdomains'),
@@ -54,37 +54,169 @@ class TestServicesCreate(base.TestCase):
     def setUp(self, mock_set_credentials):
         super(TestServicesCreate, self).setUp()
         provider = driver.DNSProvider(self.conf)
+        self.client = mock.Mock()
         self.controller = provider.services_controller
+        self.controller.client = self.client
 
-    def test_create_with_provider_failure(self):
-        if six.PY2:
-            responders = [{
-                'Fastly':
-                {
-                    'error_detail': 'Error in create',
-                    'error': 'failed to create service'
-                }
+    def test_create_with_no_links(self):
+        responders = [{
+            'Akamai': {
+                'id': u'4PRhL3lHlZhrXr1mJUt24M',
+                'links': []
+            },
+            'Fastly': {
+                'id': u'6HGJjRhdsDjDSkfGSjdsKD',
+                'links': []
+            }
+        }]
+
+        subdomain = mock.Mock()
+        subdomain.add_records = mock.Mock()
+        self.client.find = mock.Mock(return_value=subdomain)
+        dns_details = self.controller.create(responders)
+
+        for responder in responders:
+            for provider_name in responder:
+                self.assertEqual([], dns_details[provider_name]['access_urls'])
+
+    def test_create_with_provider_error(self):
+
+        responders = [{
+            'Akamai': {
+                'error': 'Create service failed with Akamai',
+                'error_detail': 'Error details'
+                },
+            'Fastly': {
+                'id': u'4PRhL3lHlZhrXr1mJUt24M',
+                'links': [
+                    {
+                        'domain': u'blog.mocksite.com',
+                        'href': u'blog.mocksite.com.global.prod.fastly.net',
+                        'rel': 'access_url'
+                    },
+                    {
+                        'domain': u'test.mocksite.com',
+                        'href': u'test.mocksite.com.global.prod.fastly.net',
+                        'rel': 'access_url'
+                    }
+                ]}
             }]
-            self.controller.create(responders)
 
-    def test_create(self):
+        subdomain = mock.Mock()
+        subdomain.add_records = mock.Mock()
+        self.client.find = mock.Mock(return_value=subdomain)
+        dns_details = self.controller.create(responders)
+
+        for responder in responders:
+            for provider_name in responder:
+                self.assertIsNotNone(dns_details[provider_name]['error'])
+                self.assertIsNotNone(
+                    dns_details[provider_name]['error_detail'])
+
+    def test_create_with_subdomain_not_found_exception(self):
+        domain_names = [u'blog.mocksite.com', u'test.mocksite.com']
         responders = [{
             'Fastly': {
                 'id': u'4PRhL3lHlZhrXr1mJUt24M',
                 'links': [
                     {
-                        'domain': u'blog.mocksite4.com',
-                        'href': u'blog.mocksite4.com.global.prod.fastly.net',
+                        'domain': u'blog.mocksite.com',
+                        'href': u'blog.mocksite.com.global.prod.fastly.net',
                         'rel': 'access_url'
                     },
                     {
-                        'domain': u'test.mocksite4.com',
-                        'href': u'test.mocksite4.com.global.prod.fastly.net',
+                        'domain': u'test.mocksite.com',
+                        'href': u'test.mocksite.com.global.prod.fastly.net',
                         'rel': 'access_url'
                     }
                 ]}
-        }]
-        self.controller.create(responders)
+            }]
+
+        self.client.find = mock.Mock(
+            side_effect=exc.NotFound('Subdomain not found'))
+        dns_details = self.controller.create(responders)
+
+        access_urls_map = {}
+        for provider_name in dns_details:
+            access_urls_map[provider_name] = {}
+            access_urls_list = dns_details[provider_name]['access_urls']
+            for access_urls in access_urls_list:
+                access_urls_map[provider_name][access_urls['domain']] = (
+                    access_urls['operator_url'])
+
+        for responder in responders:
+            for provider_name in responder:
+                for domain_name in domain_names:
+                    self.assertIsNotNone(
+                        access_urls_map[provider_name][domain_name])
+
+    def test_create_with_generic_exception(self):
+        responders = [{
+            'Fastly': {
+                'id': u'4PRhL3lHlZhrXr1mJUt24M',
+                'links': [
+                    {
+                        'domain': u'blog.mocksite.com',
+                        'href': u'blog.mocksite.com.global.prod.fastly.net',
+                        'rel': 'access_url'
+                    },
+                    {
+                        'domain': u'test.mocksite.com',
+                        'href': u'test.mocksite.com.global.prod.fastly.net',
+                        'rel': 'access_url'
+                    }
+                ]}
+            }]
+
+        subdomain = mock.Mock()
+        subdomain.add_records = mock.Mock(
+            side_effect=exc.NotFound('Subdomain not found'))
+        self.client.find = mock.Mock(return_value=subdomain)
+        dns_details = self.controller.create(responders)
+
+        for responder in responders:
+            for provider_name in responder:
+                self.assertIsNotNone(dns_details[provider_name]['error'])
+                self.assertIsNotNone(
+                    dns_details[provider_name]['error_detail'])
+
+    def test_create(self):
+        domain_names = [u'blog.mocksite.com', u'test.mocksite.com']
+        responders = [{
+            'Fastly': {
+                'id': u'4PRhL3lHlZhrXr1mJUt24M',
+                'links': [
+                    {
+                        'domain': u'blog.mocksite.com',
+                        'href': u'blog.mocksite.com.global.prod.fastly.net',
+                        'rel': 'access_url'
+                    },
+                    {
+                        'domain': u'test.mocksite.com',
+                        'href': u'test.mocksite.com.global.prod.fastly.net',
+                        'rel': 'access_url'
+                    }
+                ]}
+            }]
+
+        subdomain = mock.Mock()
+        subdomain.add_records = mock.Mock()
+        self.client.find = mock.Mock(return_value=subdomain)
+        dns_details = self.controller.create(responders)
+
+        access_urls_map = {}
+        for provider_name in dns_details:
+            access_urls_map[provider_name] = {}
+            access_urls_list = dns_details[provider_name]['access_urls']
+            for access_urls in access_urls_list:
+                access_urls_map[provider_name][access_urls['domain']] = (
+                    access_urls['operator_url'])
+
+        for responder in responders:
+            for provider_name in responder:
+                for domain_name in domain_names:
+                    self.assertIsNotNone(
+                        access_urls_map[provider_name][domain_name])
 
 
 @ddt.ddt
