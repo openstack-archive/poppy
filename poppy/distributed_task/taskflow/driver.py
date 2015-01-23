033c7f95b6c12301ee0c7f2e08c24d223f5e447d
tonytan4ever <tonytan198211@gmail.com>
2015-01-23 16:24:27 -0500

    Distributed Task Queue Driver Minimal
    Change-Id: Ic64ea2a1f3cb0b8e0b5fa952c39ba95884958e17

---
+++ poppy/distributed_task/taskflow/driver.py
@@ -0,0 +1,102 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from oslo.config import cfg
+from taskflow.jobs import backends as job_backends
+from taskflow.persistence import backends as persistence_backends
+
+from poppy.distributed_task import base
+from poppy.distributed_task.taskflow import controllers
+
+
+LOG = logging.getLogger(__name__)
+
+TASKFLOW_OPTIONS = [
+    cfg.StrOpt('jobboard_backend_type', default='zookeeper',
+               help='Default jobboard backend type'),
+    cfg.StrOpt('persistent_backend_type', default='zookeeper',
+               help='Default jobboard persistent backend type'),
+    cfg.ListOpt('jobboard_backend_host', default=['localhost'],
+                help='default jobboard backend server host'),
+    cfg.IntOpt('jobboard_backend_port', default=2181, help='default'
+               ' jobboard backend server port (e.g: ampq)'),
+    cfg.ListOpt('persistent_backend_host', default=['localhost'],
+                help='default persistent backend server host'),
+    cfg.IntOpt('persistent_backend_port', default=2181, help='default'
+               ' default persistent backend server port (e.g: ampq)'),
+]
+
+TASKFLOW_GROUP = 'drivers:distributed_task:taskflow'
+
+
+class TaskFlowDistributedTaskDriver(base.Driver):
+    """TaskFlow distributed task Driver."""
+
+    def __init__(self, conf):
+        super(TaskFlowDistributedTaskDriver, self).__init__(conf)
+        conf.register_opts(TASKFLOW_OPTIONS, group=TASKFLOW_GROUP)
+        self.distributed_task_conf = conf[TASKFLOW_GROUP]
+
+        jobbackends_hosts = ','.join(['%s:%s' % (
+            host, self.distributed_task_conf.jobboard_backend_port)
+            for host in
+            self.distributed_task_conf.jobboard_backend_host])
+        self.jobboard_backend_conf = {
+            # This topic could become more complicated
+            "board": self.distributed_task_conf.jobboard_backend_type,
+            "hosts": jobbackends_hosts,
+            "path": "/taskflow/jobs/poppy_service_jobs",
+        }
+
+        persistencebackends_hosts = ','.join(['%s:%s' % (
+            host, self.distributed_task_conf.jobboard_backend_port)
+            for host in
+            self.distributed_task_conf.jobboard_backend_host])
+        self.persistence_backend_conf = {
+            # This topic could become more complicated
+            "connection": self.distributed_task_conf.persistent_backend_type,
+            "hosts": persistencebackends_hosts,
+        }
+
+    def is_alive(self):
+        """Health check for TaskFlow worker."""
+        return True
+
+    def persistence(self):
+        return persistence_backends.backend(
+            self.persistence_backend_conf.copy())
+
+    def job_board(self, conf, persistence, **kwargs):
+        return job_backends.backend(
+            'poppy_service_jobs',
+            conf.copy(), persistence=persistence)
+
+    @property
+    def vendor_name(self):
+        """storage name.
+
+        :returns 'TaskFlow'
+        """
+        return 'TaskFlow'
+
+    @property
+    def services_controller(self):
+        """services_controller.
+
+        :returns service controller
+        """
+        return controllers.ServicesController(self)
---
+++ poppy/distributed_task/taskflow/flow/delete_service.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import os
+import sys
+
+from oslo.config import cfg
+from taskflow.patterns import linear_flow
+from taskflow import task
+
+from poppy import bootstrap
+from poppy.transport.pecan.models.request import (
+    provider_details as req_provider_details
+)
+
+
+logging.basicConfig(level=logging.ERROR,
+                    format='%(levelname)s: %(message)s',
+                    stream=sys.stdout)
+
+LOG = logging.getLogger('Poppy Service Tasks')
+LOG.setLevel(logging.DEBUG)
+
+
+conf = cfg.CONF
+conf(project='poppy', prog='poppy', args=[])
+
+
+def service_delete_task_func(provider_details,
+                             project_id, service_id):
+    bootstrap_obj = bootstrap.Bootstrap(conf)
+    service_controller = bootstrap_obj.manager.services_controller
+    provider_details = json.loads(provider_details)
+
+    responders = []
+    # try to delete all service from each provider presented
+    # in provider_details
+    for provider in provider_details:
+        provider_details[provider] = (
+            req_provider_details.load_from_json(provider_details[provider]))
+        LOG.info('Starting to delete service from %s' % provider)
+        responder = service_controller.provider_wrapper.delete(
+            service_controller._driver.providers[provider.lower()],
+            provider_details)
+        responders.append(responder)
+        LOG.info('Deleting service from %s complete...' % provider)
+
+    # delete associated cname records from DNS
+    dns_responder = service_controller.dns_controller.delete(provider_details)
+
+    for responder in responders:
+        provider_name = list(responder.items())[0][0]
+
+        if 'error' in responder[provider_name]:
+            LOG.info('Delete service from %s failed' % provider_name)
+            LOG.info('Updating provider detail status of %s for %s' %
+                     (provider_name, service_id))
+            # stores the error info for debugging purposes.
+            provider_details[provider_name].error_info = (
+                responder[provider_name].get('error_info'))
+        elif 'error' in dns_responder[provider_name]:
+            LOG.info('Delete service from DNS failed')
+            LOG.info('Updating provider detail status of %s for %s'.format(
+                     (provider_name, service_id)))
+            # stores the error info for debugging purposes.
+            provider_details[provider_name].error_info = (
+                dns_responder[provider_name].get('error_info'))
+        else:
+            # delete service successful, remove this provider detail record
+            del provider_details[provider_name]
+
+        service_controller.storage_controller._driver.connect()
+
+    if provider_details != {}:
+        # Store failed provider details with error infomation for further
+        # action, maybe for debug and/or support.
+        LOG.info('Delete failed for one or more providers'
+                 'Updating poppy service provider details for %s' %
+                 service_id)
+        service_controller.storage_controller.update_provider_details(
+            project_id,
+            service_id,
+            provider_details)
+
+    # always delete from Poppy.  Provider Details will contain
+    # any provider issues that may have occurred.
+    LOG.info('Deleting poppy service %s from all providers successful'
+             % service_id)
+    service_controller.storage_controller.delete(project_id, service_id)
+    service_controller.storage_controller._driver.close_connection()
+    LOG.info('Deleting poppy service %s succeeded' % service_id)
+    LOG.info('Delete service worker process %s complete...' %
+             str(os.getpid()))
+
+
+class DeleteServiceTask(task.Task):
+    default_provides = "service_deleted"
+
+    def execute(self, provider_details, project_id, service_id):
+        LOG.info('Start executing delete service task...')
+        service_delete_task_func(provider_details,
+                                 project_id, service_id)
+        return True
+
+
+def delete_service():
+    flow = linear_flow.Flow('Deleting poppy-service').add(
+        DeleteServiceTask(),
+    )
+    return flow
