cd2fe9ea4cf03bd821e7674b12543b996ce0a744
sabeensyed <sabeen.syed@rackspace.com>
2015-02-11 18:13:34 -0600

    Add a baseline performance test for poppy.
    Change-Id: I2c53784fc4bd05feb173dcaab2e3874df056f5a8

---
+++ tests/performance/perf_config.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2014 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding:utf-8
+host = "https://preview.cdn.api.rackspacecloud.com/v1.0"
+min_wait = 1000
+max_wait = 1000
+
+project_id = '862456'
+token = '07c54098666f4026b319f07cde747769'
+
+create_service_weight = 100
+update_service_weight = 5
+delete_service_weight = 5
+delete_asset_weight = 50
+delete_all_assets_weight = 50
+list_services_weight = 100
+get_service_weight = 50
+list_flavors_weight = 20
+get_flavors_weight = 20
+
+flavors = ['cdn', 'երանգ']
