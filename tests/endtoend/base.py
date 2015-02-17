91d81259cdcf659f519b047d9c20400fcc2f291e
Obulpathi <obulapathi.challa@rackspace.com>
2015-02-17 14:46:12 -0500

    Fixes bug caused by BeautifulSoup interface update
    Change-Id: I2560dcc182994f5302b6ed14947a69fe5ed733f4
    Closes-Bug: 1415543

--- tests/endtoend/base.py
+++ tests/endtoend/base.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import BeautifulSoup
+from bs4 import BeautifulSoup
 from cafe.drivers.unittest import fixtures
 import requests
 
