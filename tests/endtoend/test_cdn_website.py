9b62840feef3e4afe2d462a5761413c03ef11060
Malini Kamalambal <malini.kamalambal@rackspace.com>
2015-03-06 11:42:35 -0500

    End to End tests for shared cert SSL.
    Change-Id: Ie5aee2eebf791a41cf77d41899150375f8c1b8b0

--- tests/endtoend/test_cdn_website.py
+++ tests/endtoend/test_cdn_website.py
@@ -15,19 +15,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import random
-import string
-
 from tests.endtoend import base
 
 
 class TestWebsiteCDN(base.TestBase):
 
     """Tests for CDN enabling a website."""
-
-    def _random_string(self, length=12):
-        return ''.join([random.choice(string.ascii_letters)
-                        for _ in range(length)])
 
     def setUp(self):
         super(TestWebsiteCDN, self).setUp()
@@ -79,6 +72,7 @@ class TestWebsiteCDN(base.TestBase):
         self.dns_client.wait_cname_propagation(
             target=self.test_domain,
             retry_interval=self.dns_config.retry_interval)
+
         self.assertSameContent(origin_url=origin_url,
                                cdn_url=cdn_enabled_url)
 
@@ -100,3 +94,63 @@ class TestWebsiteCDN(base.TestBase):
         self.poppy_client.delete_service(location=self.service_location)
         # self.dns_client.delete_record(self.cname_rec)
         super(TestWebsiteCDN, self).tearDown()
+
+
+class TestSSLCDN(base.TestBase):
+
+    """Tests for CDN enabling with SSL on."""
+
+    def setUp(self):
+        super(TestSSLCDN, self).setUp()
+
+        if self.test_config.run_ssl_tests is False:
+            self.skipTest('SSL tests are currently disabled in configuration')
+
+        self.stack_name = self._random_string()
+        self.test_domain = 'TestCDN-SSL' + self._random_string()
+        self.origin = self.test_config.ssl_origin
+
+    def test_shared_ssl_enable_cdn(self):
+
+        # Create a Poppy Service for the test website
+        domain_list = [{"domain": self.test_domain, "protocol": "https",
+                        "certificate": "shared"}]
+        origin_list = [{"origin": self.origin, "port": 443, "ssl": True}]
+        caching_list = []
+        self.service_name = 'testService-' + self._random_string()
+
+        resp = self.poppy_client.create_service(
+            service_name=self.service_name,
+            domain_list=domain_list,
+            origin_list=origin_list,
+            caching_list=caching_list,
+            flavor_id=self.poppy_config.flavor)
+
+        self.assertEqual(resp.status_code, 202)
+        service_location = resp.headers['location']
+        self.poppy_client.wait_for_service_status(
+            location=service_location,
+            status='DEPLOYED',
+            abort_on_status='FAILED',
+            retry_interval=self.poppy_config.status_check_retry_interval,
+            retry_timeout=self.poppy_config.status_check_retry_timeout)
+
+        resp = self.poppy_client.get_service(location=service_location)
+        links = resp.json()['links']
+        origin_url = 'http://' + self.origin
+        access_url = [link['href'] for link in links if
+                      link['rel'] == 'access_url']
+        cdn_url = 'https://' + access_url[0]
+
+        self.assertSameContent(origin_url=origin_url,
+                               cdn_url=cdn_url)
+
+        # Benchmark page load metrics for the CDN enabled website
+        if self.test_config.webpagetest_enabled:
+            wpt_test_results = self.run_webpagetest(
+                url=access_url, locations=self.wpt_config.test_locations)
+            print(wpt_test_results)
+
+    def tearDown(self):
+        self.poppy_client.delete_service(service_name=self.service_name)
+        super(TestSSLCDN, self).tearDown()
