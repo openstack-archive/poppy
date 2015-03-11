d37a573174ce0825473e5f3aef5ad8fe69479c84
tonytan4ever <tonytan198211@gmail.com>
2015-03-11 12:04:41 -0400

    Geo restriciton implementation Implements blueprint: geo-restrictions
    Change-Id: I5caa88cc140c2b9b2b8429084484e16a9eb42f24

--- poppy/storage/cassandra/services.py
+++ poppy/storage/cassandra/services.py
@@ -646,7 +646,6 @@ class ServicesController(base.ServicesController):
         restrictions = [restriction.Restriction(
             r.get('name'),
             r.get('access'),
-
             [rule.Rule(r_rule.get('name'),
                        referrer=r_rule.get('referrer'),
                        client_ip=r_rule.get('client_ip'),
