91bc93996f5707fe9a0a00fdc024176e595a51c8
tonytan4ever <tonytan198211@gmail.com>
2015-03-11 12:04:41 -0400

    Change restriction model
    Change-Id: I7a2ff7f5747fdb32890635d4e63379a05f863070

--- tests/api/services/test_services.py
+++ tests/api/services/test_services.py
@@ -483,6 +483,7 @@ class TestServicePatch(base.TestBase):
 
         self.restrictions_list = [
             {"name": "website only",
+             "type": "whitelist",
              "rules": [{"name": "mywebsite.com",
                         "referrer": "www.mywebsite.com",
                         "request_url": "/*"
