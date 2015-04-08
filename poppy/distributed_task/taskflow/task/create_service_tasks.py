a3d8f1ca4f5522112bff38ac2f6199d3bed6296b
Obulpathi <obulapathi.challa@rackspace.com>
2015-04-08 18:49:27 -0400

    Enables creating container for log_delivery
    Change-Id: Ifbabec501a1f8578d5ed5e97feda8e144f93f468

--- poppy/distributed_task/taskflow/task/create_service_tasks.py
+++ poppy/distributed_task/taskflow/task/create_service_tasks.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import json
+import requests
 import time
 
 from oslo.config import cfg
@@ -27,6 +28,17 @@ from poppy.openstack.common import log
 LOG = log.getLogger(__name__)
 conf = cfg.CONF
 conf(project='poppy', prog='poppy', args=[])
+
+LOG_DELIVERY_OPTIONS = [
+    cfg.StrOpt('identity_url', default='http://identity.com',
+               help='Identity URL for swiftfs container'),
+    cfg.StrOpt('container_name', default='.CDN_ACCESS_LOGS',
+               help='Swiftfs contailer to put logs'),
+    cfg.StrOpt('preferred_dcs', default='DC1 DC2',
+               help='Preferred DCs to create container'),
+]
+
+LOG_DELIVERY_GROUP = 'log_delivery'
 
 
 class CreateProviderServicesTask(task.Task):
@@ -84,10 +96,72 @@ class CreateServiceDNSMappingTask(task.Task):
         time.sleep(retry_sleep_time)
 
 
+class CreateLogDeliveryContainerTask(task.Task):
+    default_provides = "log_responders"
+
+    def execute(self, project_id, auth_token, service_id):
+        bootstrap_obj = bootstrap.Bootstrap(conf)
+        service_controller = bootstrap_obj.manager.services_controller
+        storage_controller = service_controller.storage_controller
+
+        conf.register_opts(LOG_DELIVERY_OPTIONS, group=LOG_DELIVERY_GROUP)
+        identity_url = conf['log_delivery']['identity_url']
+        container_name = conf['log_delivery']['container_name']
+        preferred_dcs = conf['log_delivery']['preferred_dcs'].split()
+
+        try:
+            service_obj = storage_controller.get(project_id, service_id)
+            storage_controller._driver.close_connection()
+        except ValueError:
+            msg = 'Creating service {0} from Poppy failed. ' \
+                  'No such service exists'.format(service_id)
+            LOG.info(msg)
+            raise Exception(msg)
+
+
+        log_responders = []
+        # if log delivery is not enabled, return
+        """
+        if not service_obj.log_delivery:
+            return log_responders
+        """
+        # log delivery enabled, create log delivery container for the user
+
+        payload = {
+            "auth": {
+                "tenantName": project_id,
+                "token": {
+                    "id": auth_token
+                }
+            }
+        }
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(identity_url, data=json.dumps(payload), headers=headers)
+
+        catalog = r.json()
+        services = catalog['access']['serviceCatalog']
+
+        for service in services:
+            if service['name'] == 'cloudFiles':
+                endpoints = service['endpoints']
+                for endpoint in endpoints:
+                    if endpoint['region'] in preferred_dcs:
+                        swifturl = endpoint['publicURL']
+
+        container_url = '{0}/{1}'.format(swifturl, container_name)
+        headers = {'Content-Type': 'application/json',
+                   'X-Auth-Token': auth_token}
+        LOG.info('Starting to create container {0}'.format(container_url))
+        response = requests.put(container_url, None, headers=headers)
+        LOG.info('Created container {0}'.format(container_url))
+        log_responders = [container_url]
+        return log_responders
+
+
 class GatherProviderDetailsTask(task.Task):
     default_provides = "provider_details_dict"
 
-    def execute(self, responders, dns_responder):
+    def execute(self, responders, dns_responder, log_responders):
         provider_details_dict = {}
         for responder in responders:
             for provider_name in responder:
@@ -111,6 +185,7 @@ class GatherProviderDetailsTask(task.Task):
                             error_message=error_msg))
                 else:
                     access_urls = dns_responder[provider_name]['access_urls']
+                    access_urls.append({'log_delivery': log_responders[0]})
                     provider_details_dict[provider_name] = (
                         provider_details.ProviderDetail(
                             provider_service_id=responder[provider_name]['id'],
