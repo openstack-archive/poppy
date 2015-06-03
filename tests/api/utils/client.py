7970337d28b354075112b0abad71372159783024
Malini Kamalambal <malini.kamalambal@rackspace.com>
2015-06-03 11:23:56 -0400

    Add API tests for POST action on service.
    This patch adds the API tests needed for service actions per
    project_id.
    
    Change-Id: I6073a08421b39ebd753ceea6b35fe8f952f3f837

--- tests/api/utils/client.py
+++ tests/api/utils/client.py
@@ -157,6 +157,21 @@ class PoppyClient(client.AutoMarshallingHTTPClient):
 
         return self.request('DELETE', location)
 
+    def admin_service_action(self, project_id, action,
+                             requestslib_kwargs=None):
+        """Update Tenant State
+
+        :return: Response Object containing response code 202
+        POST
+        /admin/services/action
+        """
+
+        url = '/admin/services/action'
+        request_object = requests.ServiceAction(
+            project_id=project_id, action=action)
+        return self.request('POST', url, request_entity=request_object,
+                            requestslib_kwargs=requestslib_kwargs)
+
     def check_health(self):
         """Check Health of the application
 
@@ -253,14 +268,14 @@ class PoppyClient(client.AutoMarshallingHTTPClient):
             body = service.json()
             current_status = body['status']
             if (current_status.lower() == status.lower()):
-                return
+                return service
 
             if abort_on_status is not None:
                 if current_status == abort_on_status:
                     # this is for debugging purpose,
                     # will be removed later, so simply use print
                     print(body.get('errors', []))
-                    return
+                    return service
 
             current_time = int(time.time())
             if current_time > stop_time:
