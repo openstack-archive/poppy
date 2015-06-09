1cf6d0dfc919313891f9c23137428d7623ce286b
Anantha Arunachalam <anantha.arunachalam@rackspace.com>
2015-06-09 14:41:43 -0400

    Added support for HostHeaderType and HostHeaderValue while creating the service.
    Change-Id: I7fa82c7699cd300e1eb1966bbf3d25c7ab549ed6
    Implements: blueprint host-header-forwarding

--- poppy/provider/akamai/services.py
+++ poppy/provider/akamai/services.py
@@ -479,6 +479,13 @@ class ServiceController(base.ServiceBase):
             }
         }
 
+        if origin.hostheadertype.lower() == 'origin':
+            origin_behavior_dict['hostHeaderType'] = 'origin'
+            origin_behavior_dict['hostHeaderValue'] = origin.origin
+        elif origin.hostheadertype.lower() == 'custom':
+            origin_behavior_dict['hostHeaderType'] = 'custom'
+            origin_behavior_dict['hostHeaderValue'] = origin.hostheadervalue
+
         wildcards = []
 
         # this is the global 'url-wildcard' rule
