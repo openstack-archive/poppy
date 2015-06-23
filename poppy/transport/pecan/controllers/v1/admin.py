ac6ca6bda3fc4009fd56ed6fba83ca548930e062
Anantha Arunachalam <anantha.arunachalam@rackspace.com>
2015-06-23 11:34:27 -0400

    Adds an admin API for migrating the SAN domain.
    Change-Id: Ia5dd9e3dab68d38c8c4cc9d0333769362e6f03c2

--- poppy/transport/pecan/controllers/v1/admin.py
+++ poppy/transport/pecan/controllers/v1/admin.py
@@ -18,17 +18,89 @@ import json
 import pecan
 from pecan import hooks
 
+from poppy.dns.rackspace import services as dns_services
+from poppy.dns.rackspace import driver as dns_driver
 from poppy.transport.pecan.controllers import base
 from poppy.transport.pecan import hooks as poppy_hooks
 from poppy.transport.validators import helpers
+from poppy.transport.validators.schemas import san_migration
 from poppy.transport.validators.schemas import service_state
 from poppy.transport.validators.stoplight import decorators
 from poppy.transport.validators.stoplight import helpers as stoplight_helpers
 from poppy.transport.validators.stoplight import rule
 
 
-class OperatorStateController(base.Controller, hooks.HookController):
+class SANDomainMigrationController(base.Controller, hooks.HookController):
+    __hooks__ = [poppy_hooks.Context(), poppy_hooks.Error()]
 
+    def __init__(self, driver):
+        super(SANDomainMigrationController, self).__init__(driver)
+
+    @pecan.expose('json')
+    @decorators.validate(
+        request=rule.Rule(
+            helpers.json_matches_service_schema(
+                san_migration.SANMigrationServiceSchema.get_schema(
+                    "SAN_migration", "POST")),
+            helpers.abort_with_message,
+            stoplight_helpers.pecan_getter))
+    def post(self):
+        request_json = json.loads(pecan.request.body.decode('utf-8'))
+        project_id = request_json.get('project_id', None)
+        service_id = request_json.get('service_id', None)
+        domain_name = request_json.get('domain_name', None)
+        new_cert = request_json.get('new_cert', None)
+
+        if not "edgekey.net" in new_cert:
+            new_cert = new_cert + ".edgekey.net"
+
+        services_controller = self._driver.manager.services_controller
+        dns_controller = self._driver._manager.dns.services_controller
+        storage_controller = self._driver._manager.storage.services_controller
+
+        try:
+            access_url = ''
+            service_obj = services_controller.get(project_id, service_id)
+            for provider_name in service_obj.provider_details:
+                provider_detail = service_obj.provider_details[provider_name]
+                access_urls = provider_detail.access_urls
+                for url in access_urls:
+                    if 'operator_url' in url:
+                        access_url = url['operator_url']
+                        dns_controller.migrate_SAN_domain(access_url, new_cert)
+
+            # Update provider_details in cassandra
+            provider_details = storage_controller.get_provider_details(
+                project_id, service_id)
+            for provider in provider_details:
+                if str(provider_details[provider].access_urls[0][
+                           'operator_url']) == access_url:
+                    provider_details[provider].access_urls[0][
+                        'provider_url'] = new_cert
+                    storage_controller.update_provider_details(project_id,
+                                                               service_id,
+                                                               provider_details
+                                                               )
+        except ValueError:
+            pecan.abort(404, detail='Service {0} could not be found'.format(
+                service_id))
+
+        return pecan.Response(None, 202)
+
+
+class AkamaiController(base.Controller, hooks.HookController):
+    def __init__(self, driver):
+        super(AkamaiController, self).__init__(driver)
+        self.__class__.service = SANDomainMigrationController(driver)
+
+
+class ProviderController(base.Controller, hooks.HookController):
+    def __init__(self, driver):
+        super(ProviderController, self).__init__(driver)
+        self.__class__.akamai = AkamaiController(driver)
+
+
+class OperatorStateController(base.Controller, hooks.HookController):
     __hooks__ = [poppy_hooks.Context(), poppy_hooks.Error()]
 
     def __init__(self, driver):
@@ -57,7 +129,7 @@ class OperatorStateController(base.Controller, hooks.HookController):
                                              service_state)
         except ValueError:
             pecan.abort(404, detail='Service {0} could not be found'.format(
-                        service_id))
+                service_id))
 
         return pecan.Response(None, 202)
 
@@ -72,3 +144,4 @@ class AdminController(base.Controller, hooks.HookController):
     def __init__(self, driver):
         super(AdminController, self).__init__(driver)
         self.__class__.services = AdminServiceController(driver)
+        self.__class__.provider = ProviderController(driver)
