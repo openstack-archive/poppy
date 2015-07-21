e90ec6169223d087e078238a3343afc2e7f2ec5c
amitgandhinz <amit.gandhi@rackspace.com>
2015-07-21 17:52:30 -0400

    Added Docker Files to mimic third party API's
    This change works towards making the API tests pass
    against Mimic'd drivers so that real production accounts
    do not need to be set up - namely:
    - Mimic - Fastly
    - Mimic - Keystone
    
    It also makes the mimic fastly instance the default driver in /etc
    
    Implements: bp/fastly-mimic
    
    Change-Id: I35d36fbc2452fc4e2233c19abefd738c9975edd0

---
+++ run_poppy.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+DAEMONIZED=false
+
+while getopts ":d" opt; do
+  case $opt in
+    d)
+      DAEMONIZED=true
+      ;;
+  esac
+done
+
+
+pip install docker-compose
+
+# remove any running processes
+./kill_poppy.sh
+
+# remove existing containers
+docker kill compose_cassandra_1
+docker kill compose_zookeeper_1
+docker rm compose_cassandra_1
+docker rm compose_zookeeper_1
+
+# start new containers
+docker-compose -f docker/compose/dependencies.yml up -d 
+
+is_cassandra_ready() {
+    nc -z dockerhost 9042
+}
+
+is_zookeeper_ready() {
+    nc -z dockerhost 2181
+}
+
+# wait until cassandra is ready
+while ! is_cassandra_ready -eq 1
+do
+  echo "still trying to connect to cassandra"
+  sleep 1
+done
+echo "connected successfully to cassandra"
+
+
+# wait until zookeeper is ready
+while ! is_zookeeper_ready -eq 1
+do
+  echo "still trying to connect to zookeeper"
+  sleep 1
+done
+echo "connected successfully to zookeeper"
+
+# start the poppy-workers
+exec poppy-worker > /dev/null 2>&1 &
+exec poppy-worker > /dev/null 2>&1 &
+exec poppy-worker > /dev/null 2>&1 &
+
+exec poppy-worker > /dev/null 2>&1 &
+exec poppy-worker > /dev/null 2>&1 &
+exec poppy-worker > /dev/null 2>&1 &
+
+exec poppy-worker > /dev/null 2>&1 &
+exec poppy-worker > /dev/null 2>&1 &
+exec poppy-worker > /dev/null 2>&1 &
+
+# start the poppy-server
+if $DAEMONIZED; then
+    exec poppy-server > /dev/null 2>&1 &
+else
+    exec poppy-server
+fi
+
+
+
+echo "Poppy Server and Workers Started"
