ec8aeac43380c02287604661a1d18fb96d681f09
tonytan4ever <tonytan198211@gmail.com>
2015-08-19 16:22:14 -0400

    Fixing api test (restriction with field access as oppose to type)
    Change-Id: I7d4b519e6a7e95eb782fc01b53987f5b8e33e958

--- tests/api/services/test_patch_service.py
+++ tests/api/services/test_patch_service.py
@@ -83,7 +83,7 @@ class TestServicePatch(base.TestBase):
 
         self.restrictions_list = [
             {"name": "website only",
-             "type": "whitelist",
+             "access": "whitelist",
              "rules": [{"name": "mywebsite.com",
                         "referrer": "www.mywebsite.com",
                         "request_url": "/*"
@@ -137,8 +137,8 @@ class TestServicePatch(base.TestBase):
         # Default restriciton to whitelist
         expected_restrictions = expected_service_details['restrictions']
         for restriction in expected_restrictions:
-            if 'type' not in restriction:
-                restriction['type'] = 'whitelist'
+            if 'access' not in restriction:
+                restriction['access'] = 'whitelist'
 
         resp = self.client.patch_service(location=self.service_url,
                                          request_body=test_data)
