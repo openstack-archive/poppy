1c9f1af7c6dea8c4fba38f86f028c02efd74c325
tonytan4ever <tonytan198211@gmail.com>
2015-08-20 12:19:15 -0400

    restriction type change to access in add_restrictions test case, and in python code
    Change-Id: I16d0cb7d38c902d9e2b1846ad4fdb91b2458c483

--- tests/api/services/test_patch_service.py
+++ tests/api/services/test_patch_service.py
@@ -371,7 +371,7 @@ class TestServicePatchWithLogDelivery(base.TestBase):
 
         self.restrictions_list = [
             {"name": "website only",
-             "type": "whitelist",
+             "access": "whitelist",
              "rules": [{"name": "mywebsite.com",
                         "referrer": "www.mywebsite.com",
                         "request_url": "/*"
@@ -426,8 +426,8 @@ class TestServicePatchWithLogDelivery(base.TestBase):
         # Default restriction to whitelist
         expected_restrictions = expected_service_details['restrictions']
         for restriction in expected_restrictions:
-            if 'type' not in restriction:
-                restriction['type'] = 'whitelist'
+            if 'access' not in restriction:
+                restriction['access'] = 'whitelist'
 
         expected_origin = expected_service_details['origins']
         for item in expected_origin:
