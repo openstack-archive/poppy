b783f4f7d6d8f5747e826755f17e4073ba45f88e
tonytan4ever <tony.tan@rackspace.com>
2015-09-16 15:56:08 -0400

    Update PAPI property and certificate status check scripts
    Change-Id: I9211d9f24cbd46c8818993d3ff8493d5fba4bc28

---
+++ poppy/cmd/akamai_update_papi_property_for_mod_san.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2015 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+from oslo_config import cfg
+
+from poppy.common import cli
+from poppy.openstack.common import log
+from poppy.provider.akamai.background_jobs.update_property import \
+    update_property_flow
+
+LOG = log.getLogger(__name__)
+
+CLI_OPT = [
+    cfg.StrOpt('domain_name',
+               required=True,
+               help='The domain you want to add in host name (cnameFrom)'),
+    cfg.StrOpt('san_cert_name',
+               required=True,
+               help='Cert type of this cert'),
+    cfg.StrOpt('action',
+               default="add",
+               help='What kind of action, do you want "add" or "remove" '
+               'hostnames'),
+    cfg.StrOpt('property_spec',
+               default='akamai_https_san_config_numbers',
+               help='Property spec of the property to be updated'),
+]
+
+
+@cli.runnable
+def run():
+    # TODO(kgriffs): For now, we have to use the global config
+    # to pick up common options from openstack.common.log, since
+    # that module uses the global CONF instance exclusively.
+    conf = cfg.CONF
+    conf(project='poppy', prog='poppy')
+
+    conf.register_cli_opt(CLI_OPT)
+
+    LOG.info("%s: %s to %s, on property: %s" % (
+        conf.action,
+        conf.domain_name,
+        conf.san_cert_name,
+        conf.property_spec
+    ))
+
+    update_info_list = json.dumps([
+        conf.action,
+        {
+            "cnameFrome": conf.domain_name,
+            "cnameTo": '.'.join([conf.san_cert_name, 'edgekey.net']),
+            "cnameType": "EDGE_HOSTNAME"
+        }
+    ])
+
+    update_property_flow.run_update_property_flow(
+        conf.property_spec, conf.update_type, update_info_list)
