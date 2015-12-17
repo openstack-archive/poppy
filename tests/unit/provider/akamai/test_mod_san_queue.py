295b074d3d9809b219ebb51247ac259ff0515f4e
tonytan4ever <tonytan198211@gmail.com>
2015-12-17 12:31:19 -0500

    feat: Admin Akamai GET SAN retry-list endpoint Partially Implements: blueprint san-retry-list
    Change-Id: I884d920591f0193226be620c6403e820377bccec

--- tests/unit/provider/akamai/test_mod_san_queue.py
+++ tests/unit/provider/akamai/test_mod_san_queue.py
@@ -13,8 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
+
 import mock
 from oslo_config import cfg
+from zake import fake_client
 
 from poppy.provider.akamai.mod_san_queue import zookeeper_queue
 from tests.unit import base
@@ -46,8 +49,16 @@ class TestModSanQueue(base.TestCase):
             "domain_name": "www.abc.com",
             "flavor_id": "premium"
         }
+
+        # Need this fake class bc zake's fake client
+        # does not take any host parameters
+        class fake_kz_client(fake_client.FakeClient):
+            def __init__(self, hosts):
+                super(self.__class__, self).__init__()
+
         zookeeper_client_patcher = mock.patch(
-            'kazoo.client.KazooClient'
+            'kazoo.client.KazooClient',
+            fake_kz_client
         )
         zookeeper_client_patcher.start()
         self.addCleanup(zookeeper_client_patcher.stop)
@@ -55,17 +66,49 @@ class TestModSanQueue(base.TestCase):
         self.conf = cfg.ConfigOpts()
         self.zk_queue = zookeeper_queue.ZookeeperModSanQueue(self.conf)
 
-        self.zk_queue.mod_san_queue_backend = mock.Mock()
-
     def test_enqueue_mod_san_request(self):
-        self.zk_queue.enqueue_mod_san_request(self.cert_obj_json)
-        self.zk_queue.mod_san_queue_backend.put.assert_called_once_with(
-            self.cert_obj_json)
+        self.zk_queue.enqueue_mod_san_request(
+            json.dumps(self.cert_obj_json).encode('utf-8'))
+        self.assertTrue(len(self.zk_queue.mod_san_queue_backend) == 1)
+        self.assertTrue(
+            json.loads(self.zk_queue.mod_san_queue_backend.get().
+                       decode('utf-8')) == self.cert_obj_json)
 
     def test_dequeue_mod_san_request(self):
-        self.zk_queue.dequeue_mod_san_request()
-        self.zk_queue.dequeue_mod_san_request(False)
+        self.zk_queue.enqueue_mod_san_request(
+            json.dumps(self.cert_obj_json).encode('utf-8'))
+        res = self.zk_queue.dequeue_mod_san_request(False).decode('utf-8')
+        self.assertTrue(len(self.zk_queue.mod_san_queue_backend) == 1)
+        self.assertTrue(json.loads(res) == self.cert_obj_json)
 
-        calls = [mock.call(), mock.call()]
-        self.zk_queue.mod_san_queue_backend.get.assert_has_calls(calls)
-        self.zk_queue.mod_san_queue_backend.consume.assert_called_once_with()
+        res = self.zk_queue.dequeue_mod_san_request().decode('utf-8')
+        self.assertTrue(len(self.zk_queue.mod_san_queue_backend) == 0)
+        self.assertTrue(json.loads(res) == self.cert_obj_json)
+
+    def test_traverse_queue(self):
+        self.zk_queue.enqueue_mod_san_request(
+            json.dumps(self.cert_obj_json).encode('utf-8'))
+        res = self.zk_queue.traverse_queue()
+        self.assertTrue(len(res) == 1)
+        res = [json.loads(r.decode('utf-8')) for r in res]
+        self.assertTrue(res == [self.cert_obj_json])
+
+    def test_traverse_queue_multiple_records(self):
+        # Get a list of records to enqueue
+        cert_obj_list = []
+        for i in range(10):
+            cert_obj = {
+                "cert_type": "san",
+                "domain_name": "www.abc%s.com" % str(i),
+                "flavor_id": "premium",
+                "validate_service": False
+            }
+            cert_obj_list.append(cert_obj)
+
+        for cert_obj in cert_obj_list:
+            self.zk_queue.enqueue_mod_san_request(
+                json.dumps(cert_obj).encode('utf-8'))
+        res = self.zk_queue.traverse_queue()
+        self.assertTrue(len(res) == 10)
+        res = [json.loads(r.decode('utf-8')) for r in res]
+        self.assertTrue(res == cert_obj_list)
