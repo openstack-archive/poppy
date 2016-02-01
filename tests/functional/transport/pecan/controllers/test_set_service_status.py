b6e11e38c0470e0672097038e2d433424637601a
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2016-02-01 10:41:08 -0500

    feat: admin endpoint to set status of a particular service
    - REQUEST
    
      POST http://127.0.0.1:8888/v1.0/admin/services/status
    
      {
          "project_id": "98765431",
          "service_id": "e8b44506-71b7-4074-9bc8-1b5037ade112",
          "status": "deployed"
      }
    
    - RESPONSE
    
      201
    
    Change-Id: I89d40601c55af6cc3c2d45352d8e24c691107bd0

---
+++ tests/functional/transport/pecan/controllers/test_set_service_status.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2015 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import uuid
+
+import ddt
+
+from tests.functional.transport.pecan import base
+
+
+@ddt.ddt
+class TestServicesState(base.FunctionalTest):
+
+    def setUp(self):
+        super(TestServicesState, self).setUp()
+
+        self.project_id = str(uuid.uuid4())
+        self.service_id = str(uuid.uuid4())
+        self.req_body = {
+            'project_id': self.project_id,
+            'service_id': self.service_id,
+        }
+
+    @ddt.data(u'create_in_progress', u'deployed', u'update_in_progress',
+              u'delete_in_progress', u'failed')
+    def test_services_state_valid_states(self, status):
+        self.req_body['status'] = status
+        response = self.app.post(
+            '/v1.0/admin/services/status',
+            params=json.dumps(self.req_body),
+            headers={'Content-Type': 'application/json',
+                     'X-Project-ID': str(uuid.uuid4())})
+
+        self.assertEqual(response.status_code, 201)
+
+    @ddt.data('Wrong', 'On Cloud Nine')
+    def test_services_state_invalid_states(self, status):
+        # invalid status field
+        self.req_body['status'] = status
+        response = self.app.post(
+            '/v1.0/admin/services/status',
+            params=json.dumps(self.req_body),
+            headers={'Content-Type': 'application/json',
+                     'X-Project-ID': str(uuid.uuid4())},
+            expect_errors=True)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_services_state_invalid_service_id(self):
+        # invalid service_id field
+        self.req_body['status'] = 'deployed'
+        self.req_body['service_id'] = 'invalid'
+        response = self.app.post(
+            '/v1.0/admin/services/status',
+            params=json.dumps(self.req_body),
+            headers={'Content-Type': 'application/json',
+                     'X-Project-ID': str(uuid.uuid4())},
+            expect_errors=True)
+
+        self.assertEqual(response.status_code, 400)
