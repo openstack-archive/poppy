39b19d09d02189734cd0cc55bad29afdc7b0ba4e
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2016-02-09 14:48:08 -0500

    feat: allow querying for services and project_ids based on status
    The allowed query params for status are:
            'deploy_in_progress',
            'deployed',
            'update_in_progress',
            'delete_in_progress',
            'failed'
    
    REQUEST:
    
    GET /v1.0/admin/services?status=failed
    
    RESPONSE:
    
    [
      {
        "service_id": "7b68485a-1ba1-464a-93b6-9f673d637531",
        "project_id": "000"
      }
    ]
    
    200 OK
    
    Change-Id: I37b1fbf0b1339f827b707c68266fadf63fcd61fa

---
+++ tests/functional/transport/pecan/controllers/test_get_service_status.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2015 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import uuid
+
+import ddt
+from hypothesis import given
+from hypothesis import strategies
+import mock
+
+from poppy.manager.default.services import DefaultServicesController
+
+from tests.functional.transport.pecan import base
+
+
+@ddt.ddt
+class TestGetServiceStatus(base.FunctionalTest):
+
+    @given(strategies.text())
+    def test_get_service_status_invalid_queryparam(self, status):
+        # invalid status field
+        try:
+            url = '/v1.0/admin/services?status=%s' % status.decode('utf-8')
+        except (UnicodeDecodeError, UnicodeEncodeError):
+            pass
+        else:
+            response = self.app.get(url,
+                                    headers={'Content-Type':
+                                             'application/json',
+                                             'X-Project-ID':
+                                             str(uuid.uuid4())},
+                                    expect_errors=True)
+
+            self.assertEqual(response.status_code, 400)
+
+    @ddt.data('deploy_in_progress', 'deployed', 'update_in_progress',
+              'delete_in_progress', 'failed')
+    def test_get_service_status_valid_queryparam(self, status):
+        # valid status
+        with mock.patch.object(DefaultServicesController,
+                               'get_services_by_status'):
+            response = self.app.get('/v1.0/admin/services'
+                                    '?status={0}'.format(status),
+                                    headers={'Content-Type':
+                                             'application/json',
+                                             'X-Project-ID':
+                                             str(uuid.uuid4())})
+
+            self.assertEqual(response.status_code, 200)
