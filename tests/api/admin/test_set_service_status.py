9bef7170861c92d2d6ae96d6d86acf9134667926
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2016-02-09 14:48:08 -0500

    feat: allow querying for services and project_ids based on status
    The allowed query params for status are:
            'deploy_in_progress',
            'deployed',
            'update_in_progress',
            'delete_in_progress',
            'failed'
    
    REQUEST:
    
    GET /v1.0/admin/services?status=failed
    
    RESPONSE:
    
    [
      {
        "service_id": "7b68485a-1ba1-464a-93b6-9f673d637531",
        "project_id": "000"
      }
    ]
    
    200 OK
    
    Change-Id: I37b1fbf0b1339f827b707c68266fadf63fcd61fa

--- tests/api/admin/test_set_service_status.py
+++ tests/api/admin/test_set_service_status.py
@@ -18,6 +18,7 @@
 import ddt
 
 from tests.api import base
+from tests.api.utils.schema import admin
 
 
 @ddt.ddt
@@ -29,100 +30,45 @@ class TestServiceStatus(base.TestBase):
         if self.test_config.run_operator_tests is False:
             self.skipTest(
                 'Test Operator Functions is disabled in configuration')
-        self.service_name = self.generate_random_string(prefix='API-Test-')
         self.flavor_id = self.test_flavor
+        self.service_urls = []
 
-        domain = self.generate_random_string(
-            prefix='www.api-test-domain') + '.com'
-        self.domain_list = [
-            {"domain": domain}
-        ]
+    @ddt.data((1, u'deployed'), (1, u'failed'),
+              (3, u'deployed'), (3, u'failed'),
+              (5, u'deployed'), (5, u'failed'))
+    def test_set_services(self, services_status):
+        no_of_services, status = services_status
+        self.service_urls = \
+            [self._service_limit_create_test_service(client=self.client)
+             for _ in range(no_of_services)]
 
-        origin = self.generate_random_string(
-            prefix='api-test-origin') + u'.com'
-        self.origin_list = [
-            {
-                u"origin": origin,
-                u"port": 80,
-                u"ssl": False,
-                u"rules": [{
-                    u"name": u"default",
-                    u"request_url": u"/*"
-                }]
-            }
-        ]
-
-        self.caching_list = [
-            {
-                u"name": u"default",
-                u"ttl": 3600,
-                u"rules": [{
-                    u"name": "default",
-                    u"request_url": "/*"
-                }]
-            },
-            {
-                u"name": u"home",
-                u"ttl": 1200,
-                u"rules": [{
-                    u"name": u"index",
-                    u"request_url": u"/index.htm"
-                }]
-            }
-        ]
-
-        self.restrictions_list = [
-            {
-                u"name": u"website only",
-                u"rules": [
-                    {
-                        u"name": domain,
-                        u"referrer": domain,
-                        u"request_url": "/*"
-                    }
-                ]
-            }
-        ]
-
-        resp = self.setup_service(
-            service_name=self.service_name,
-            domain_list=self.domain_list,
-            origin_list=self.origin_list,
-            caching_list=self.caching_list,
-            restrictions_list=self.restrictions_list,
-            flavor_id=self.flavor_id)
-
-        self.assertEqual(resp.status_code, 202)
-        self.assertEqual(resp.text, '')
-        self.service_url = resp.headers['location']
-
-        self.client.wait_for_service_status(
-            location=self.service_url,
-            status='deployed',
-            abort_on_status='failed',
-            retry_interval=self.test_config.status_check_retry_interval,
-            retry_timeout=self.test_config.status_check_retry_timeout)
-
-    @ddt.data(u'deployed',  u'failed')
-    def test_set_service(self, status):
-
-        service_id = self.service_url.rsplit('/')[-1:][0]
+        service_ids = [url.rsplit('/')[-1:][0] for url in self.service_urls]
         project_id = self.user_project_id
 
-        set_service_resp = self.operator_client.set_service_status(
-            project_id=project_id,
-            service_id=service_id,
-            status=status)
+        for service_id, service_url in zip(service_ids, self.service_urls):
+            set_service_resp = self.operator_client.set_service_status(
+                project_id=project_id,
+                service_id=service_id,
+                status=status)
 
-        self.assertEqual(set_service_resp.status_code, 201)
+            self.assertEqual(set_service_resp.status_code, 201)
 
-        service_resp = self.client.get_service(self.service_url)
-        resp_body = service_resp.json()
-        resp_status = resp_body['status']
-        self.assertEqual(resp_status, status)
+            service_resp = self.client.get_service(service_url)
+            resp_body = service_resp.json()
+            resp_status = resp_body['status']
+            self.assertEqual(resp_status, status)
+
+            get_service_resp = self.operator_client.get_by_service_status(
+                status=status)
+
+            self.assertSchema(get_service_resp.json(),
+                              admin.get_service_project_status)
+            self.assertIn(service_id, get_service_resp.content)
+            self.assertIn(project_id, get_service_resp.content)
 
     def tearDown(self):
-        self.client.delete_service(location=self.service_url)
-        if self.test_config.generate_flavors:
-            self.client.delete_flavor(flavor_id=self.flavor_id)
-        super(TestServiceStatus, self).tearDown()
+        for service_url in self.service_urls:
+            self.client.delete_service(location=service_url)
+            if self.test_config.generate_flavors:
+                self.client.delete_flavor(flavor_id=self.flavor_id)
+            super(TestServiceStatus, self).tearDown()
